bbf5989d767486af7e2d40874e3dd2189d2054b8
clara <clara@clara.com>
2017-02-28 15:10:35 -0600

    color picker

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,30 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  req.db.collection('flowers').find().toArray(function(err, docs){
+  req.db.collection('flowers').distinct('color', function(err, colorDocs){
     if (err) {
       return next(err)
     }
-    return res.render('all_flowers', {'flowers': docs});
-  });
 
+    if (req.query.color_filter) {
+
+      req.db.collection('flowers').find({"color":req.query.color_filter}).toArray(function (err, docs) {
+        if (err) {
+          return next(err);
+        }
+        return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs, 'color_filter': req.query.color_filter});
+      });
+
+    } else {
+      req.db.collection('flowers').find().toArray(function (err, docs) {
+        if (err) {
+          return next(err);
+        }
+        return res.render('all_flowers', {'flowers': docs, 'colors': colorDocs});
+
+      });
+    }
+  });
 });
 
 module.exports = router;
