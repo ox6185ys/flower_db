5792bbe849148e2e9fbffcd61be52ce0487c5950
Patrick <ox6185ys@go.minneapolis.edu>
2017-03-23 19:12:12 -0500

    Update 3/23_4

--- notes.js
+++ notes.js
@@ -1,1 +1,13 @@
+// Week 9
 // npm install
+//mongod --dbpath yourdbpath (This starts mongod)
+
+db.flowers.remove(
+    deleteButtonPressed,
+    {
+     justOne: true,
+        writeConcern: deleteButtonPressed
+    }
+)
+
+db.flowers.drop(true);
